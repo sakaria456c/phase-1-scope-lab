0116f393fa1efa5518f38b6c9f46864270c76b49
sakaria <sakariafarah@yahoo.com>
2022-05-12 19:21:50 -0500

    commit tree b51a20d4d9e6100d9ef047f0cfb8971e7f0baac7

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution in this file!
+var customerName='bob'
+const leastFavoriteCustomer='karen'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer='steve'
+}
+function overwriteBestCustomer(){
+    return bestCustomer='maybe bob'
+}
+function upperCaseCustomerName(){
+    return customerName='BOB'
+}
+function setBestCustomer(){
+    return bestCustomer='not bob'
+}
